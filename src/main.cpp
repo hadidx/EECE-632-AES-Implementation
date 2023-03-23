3c7123ea0c4a2df26d566b77906d99888877305f
Hadi Al Khansa <hadi.alkhansa@topotrade.com>
2023-03-23 17:26:40 +0200

    Update gitignore and add example module

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include "exampleModule.h"
 
 using namespace std;
-
+using namespace examples;
 //execute actual program here
 
 int main()
 {
-
+    my_class mc;
+    mc.do_something();
 }
---
+++ src/modules/exampleModule.h
@@ -0,0 +1,9 @@
+namespace examples
+{
+    class my_class
+    {
+    public:
+        void do_something();
+    };
+
+}
