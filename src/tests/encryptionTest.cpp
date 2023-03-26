72588a47fe3c4c729cf99759c9c46a797d13849f
sylvanayakhni <silvana.yakhni@lau.edu>
2023-03-26 07:41:55 +0300

    Added Encryption for ONE round

---
+++ src/tests/encryptionTest.cpp
@@ -0,0 +1,42 @@
+#include "encryptionModule.h"
+#include "simpletest.h"
+#include <iostream>
+#include <iterator>
+
+
+using namespace AES;
+
+
+DEFINE_TEST(EncrptTestExampleOneRound)
+{
+
+    static uint8_t input[4][4] = {
+        {0x32, 0x88, 0x31, 0xe0},
+        {0x43, 0x5a, 0x31, 0x37},
+        {0xf6, 0x30, 0x98, 0x07},
+        {0xa8, 0x8d, 0xa2, 0x34}};
+
+    static uint8_t cipher_key [4][4]= {
+        {0x2b, 0x28, 0xab, 0x09},
+        {0x7e, 0xae, 0xf7, 0xcf},
+        {0x15, 0xd2, 0x15, 0x4f},
+        {0x16, 0xa6, 0x88, 0x3c}};
+
+    static uint8_t cipher_text [4][4]= {
+        {0x04, 0xe0, 0x48, 0x28},
+        {0x66, 0xcb, 0xf8, 0x06},
+        {0x81, 0x19, 0xd3, 0x26},
+        {0xe5, 0x9a, 0x7a, 0x4c}};
+
+    uint8_t output[4][4] = {Encryption::Encrypt_one_round(input, cipher_key)}; 
+
+    bool equal = std::equal(std::begin(output), std::end(output), std::begin(cipher_text));
+
+    if (equal) {
+        std::cout << "Encryption Test SUCCEDDED!" << std::endl;
+    } else {
+        std::cout << "Encryption Test Failed!" << std::endl;
+    }
+
+}
+
